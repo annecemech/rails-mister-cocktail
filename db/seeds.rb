486a8777db42e89ed740482653daa08abf422446
AnneCecile Mechin <ac.mechin@gmail.com>
2020-11-12 16:33:37 +0100

    unfinished Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "JSON"
+
+puts "Cleaning ingredients database..."
+
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_list = JSON.parse(open(url).read)["drinks"]
+
+ingredients_list.each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+puts "Seeding OK!"
